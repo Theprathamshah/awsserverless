dd9bb8eb0c2560d97900ba33296dd380ffe14763
Pratham Shah <itspratham2911@gmail.com>
2024-06-14 17:26:56 +0530

    Added lambda to create new user using AWS SDK 3

--- lambdas/addUser.js
+++ lambdas/addUser.js
@@ -1,29 +1,43 @@
-const AWS = require('aws-sdk')
+import { DynamoDBClient} from "@aws-sdk/client-dynamodb";
+import { PutCommand,DynamoDBDocumentClient} from "@aws-sdk/lib-dynamodb";
 
-const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
-exports.handler = async (event) => {
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
+
+export const handler = async (event) => {
+
+    
     const body = JSON.parse(event.body);
-    const params ={
+    const command = new PutCommand({
         TableName: 'productGroupTableDev',
         Item: {
             ID: body.ID,
             name: body.name,
             age: body.age,
-            occupation:body.occupation
+            occupation: body.occupation
         }
+    });
+
+    const response = await docClient.send(command);
+    console.log(response);
+    return {
+        statusCode: 200,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(response),
     }
-    try {
-        await dynamoDB.put(params).promise();
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ message: 'Data inserted successfully' }),
-        };
-    } catch (error) {
-        console.error(error);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ message: 'Failed to insert data', error: error.message }),
-        };
-    }
+    // try {
+    //     await dynamoDB.put(params).promise();
+    //     return {
+    //         statusCode: 200,
+    //         body: JSON.stringify({ message: 'Data inserted successfully' }),
+    //     };
+    // } catch (error) {
+    //     console.error(error);
+    //     return {
+    //         statusCode: 500,
+    //         body: JSON.stringify({ message: 'Failed to insert data', error: error.message }),
+    //     };
+    // }
 }
