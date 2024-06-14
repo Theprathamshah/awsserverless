fca9172efaf61e21e7951e2e3414abde260eea37
Pratham Shah <itspratham2911@gmail.com>
2024-06-15 00:15:19 +0530

    Added functionality of get all users using AWS sdk 3

--- lambdas/getAllUsers.js
+++ lambdas/getAllUsers.js
@@ -1,26 +1,54 @@
-const AWS = require('aws-sdk');
+// Fetch all user using aws ask 2
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-const tableName = 'productGroupTableDev';
+// const AWS = require('aws-sdk');
 
-exports.handler = async (event) => {
-    console.log('Events', event);
+// const dynamoDb = new AWS.DynamoDB.DocumentClient();
+// const tableName = 'productGroupTableDev';
 
-    const params = {
-        TableName: tableName
-    };
+// exports.handler = async (event) => {
+//     console.log('Events', event);
 
-    try {
-        const data = await dynamoDb.scan(params).promise();
-        return {
-            statusCode: 200,
-            body: {
-                message: 'All users retrieved successfully!',
-                data: data.Items
-            }
-        }
-    } catch (error) {
-        console.error('DynamoDB error: ', error);
-        return { body: { message: 'Internal Server Error', error } };
+//     const params = {
+//         TableName: tableName
+//     };
+
+//     try {
+//         const data = await dynamoDb.scan(params).promise();
+//         return {
+//             statusCode: 200,
+//             body: {
+//                 message: 'All users retrieved successfully!',
+//                 data: data.Items
+//             }
+//         }
+//     } catch (error) {
+//         console.error('DynamoDB error: ', error);
+//         return { body: { message: 'Internal Server Error', error } };
+//     }
+// };
+
+
+
+//~ Fetch all user using aws-sdk 3 !!
+
+import {DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient,ScanCommand} from "@aws-sdk/lib-dynamodb";
+
+
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
+export const handler = async () => {
+    const command = new ScanCommand({
+        TableName: "productGroupTableDev",
+    })
+    const {Items} = await docClient.send(command);
+
+    return {
+        statusCode: 200,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(Items),
     }
-};
+    
+
+}
